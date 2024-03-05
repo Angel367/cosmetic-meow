f39f442cc4efcd3f30ee4957e9d448e10c9f6b59
Egor Volkov <egor.volkov20038@gmail.com>
2024-03-05 23:47:18 +0300

    urls fixed

--- cosmetic_meow_backend/cosmetic_meow_api/urls.py
+++ cosmetic_meow_backend/cosmetic_meow_api/urls.py
@@ -12,8 +12,8 @@ router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(router.urls))
 ]
