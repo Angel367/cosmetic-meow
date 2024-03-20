042c18e0a8b23ab7a1e30a589dd68f56afd27b41
13melisa13 <isaeva.a.v@edu.mirea.ru>
2024-03-20 04:49:45 +0300

    todo fix footer, main-page

--- cosmetic_meow_frontend/src/components/error/Loading.jsx
+++ cosmetic_meow_frontend/src/components/error/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Loading() {
     return (
-            // todo: add loading animation
+
 
             <span className={"loading not-main-h1"}>
                 <span className="loading__spinner"/>
