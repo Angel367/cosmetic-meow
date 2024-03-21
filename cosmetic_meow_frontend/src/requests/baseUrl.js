bb6ea00820bf7a4706b315d3eecc055616b98215
Egor Volkov <egor.volkov20038@gmail.com>
2024-03-21 09:02:42 +0300

    ready..?

--- cosmetic_meow_frontend/src/requests/baseUrl.js
+++ cosmetic_meow_frontend/src/requests/baseUrl.js
@@ -1,5 +1,5 @@
 function getBaseUrl(isMail=false) {
-     return isMail ? "mailto:simple@mail.com" : "http://localhost/api/";
+     return isMail ? "mailto:simple@mail.com" : "http://195.58.50.137/api/";
 
 }
 
