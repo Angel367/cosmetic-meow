dfd1d36401b7f2c115d8025861c19a36098196f8
Egor Volkov <egor.volkov20038@gmail.com>
2024-03-21 17:30:55 +0300

    ready..?

--- cosmetic_meow_frontend/src/requests/baseUrl.js
+++ cosmetic_meow_frontend/src/requests/baseUrl.js
@@ -1,5 +1,5 @@
 function getBaseUrl(isMail=false) {
-     return isMail ? "mailto:simple@mail.com" : "https://195.58.50.137/api/";
+     return isMail ? "mailto:simple@mail.com" : "https://licsideris.ru/api/";
 
 }
 
