7144c94a3eecf66e0496fc83dde27324e2845913
Egor Volkov <egor.volkov20038@gmail.com>
2024-03-25 04:22:53 +0300

    added one test

--- cosmetic_meow_backend/cosmetic_meow_api/tests.py
+++ cosmetic_meow_backend/cosmetic_meow_api/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import PickUpPoint
+from .serializers import PickUpPointSerializer
 
-# Create your tests here.
+
+class PickUpPointAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.pickup_point1 = PickUpPoint.objects.create(name='Point 1', address='Address 1')
+        self.pickup_point2 = PickUpPoint.objects.create(name='Point 2', address='Address 2')
+
+    def test_list_pickup_points(self):
+        response = self.client.get('/api/pickup_point/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        pickup_points = PickUpPoint.objects.all()
+        serializer = PickUpPointSerializer(pickup_points, many=True)
+        self.assertEqual(response.data['results'], serializer.data)
+
+    def test_retrieve_pickup_point(self):
+        response = self.client.get(f'/api/pickup_point/{self.pickup_point1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        pickup_point = PickUpPoint.objects.get(id=self.pickup_point1.id)
+        serializer = PickUpPointSerializer(pickup_point)
+        self.assertEqual(response.data, serializer.data)
+
+
