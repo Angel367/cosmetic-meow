febb7702dd4a030dac95509de5004ef18785be9f
Egor Volkov <egor.volkov20038@gmail.com>
2024-04-02 05:21:41 +0300

    pre-ready env stuff (not tested, should be merged with prod/front) (4)

--- cosmetic_meow_frontend/src/requests/baseUrl.js
+++ cosmetic_meow_frontend/src/requests/baseUrl.js
@@ -1,7 +1,8 @@
-const host = process.env.REACT_HOST;
-
 function getBaseUrl(isMail = false, isPhone = false) {
+    const host = process.env.REACT_HOST;
     const baseUrl = host ? host : "http://localhost";
 
     return isMail ? "info@licsideris.ru" : `${baseUrl}/api/`;
 }
+
+export default getBaseUrl;
