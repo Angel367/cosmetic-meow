a8807ee5bbde7c424a5a6d49f4e128867dac37eb
Egor Volkov <egor.volkov20038@gmail.com>
2024-04-02 05:51:39 +0300

    pre-ready env stuff (not tested, should be merged with prod/front) (12)

--- cosmetic_meow_frontend/src/requests/baseUrl.js
+++ cosmetic_meow_frontend/src/requests/baseUrl.js
@@ -1,7 +1,5 @@
-const env = dotenv.config({ path: '.env.local' }).parsed;
-
 function getBaseUrl(isMail = false, isPhone = false) {
-    const host = process.env.REACT_HOST;
+    const host = process.env.REACT_APP_HOST;
     const baseUrl = host ? host : "http://localhost";
 
     return isMail ? "info@licsideris.ru" : `${baseUrl}/api/`;
